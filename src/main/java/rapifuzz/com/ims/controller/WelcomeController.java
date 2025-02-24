0f9c7865c52bb9b10f871d7a279932a6c2f48b1e
Piyush Kumar Singh <88372175+peyushx@users.noreply.github.com>
2025-02-24 13:18:02 +0530

    Update WelcomeController.java

--- src/main/java/rapifuzz/com/ims/controller/WelcomeController.java
+++ src/main/java/rapifuzz/com/ims/controller/WelcomeController.java
@@ -1,16 +1,10 @@
 package rapifuzz.com.ims.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rapifuzz.com.ims.model.request.IncidentRecord;
-import rapifuzz.com.ims.model.request.SearchIncidentRecord;
 import rapifuzz.com.ims.model.response.ImsResponse;
-import rapifuzz.com.ims.service.IncidentService;
 
 @RestController
 @RequestMapping("/")
